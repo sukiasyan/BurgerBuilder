d3328dcea1b8e73a76153edbeda2d3448307925c
Hakob Sukiasyan <shakob@gmail.com>
2018-02-23 23:27:04 +0400

    Mobile Toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
 
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+    sideDrawerCloseHandler = () => {
+      this.setState({showSideDrawer: false});
+    };
+    render() {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer
+                    closed={this.sideDrawerCloseHandler}
+                    open={this.state.showSideDrawer}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+export default Layout;

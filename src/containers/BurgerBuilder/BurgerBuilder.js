919b4ac07cbb67ac65783d8c70971ab50ccc1260
Hakob Sukiasyan <shakob@gmail.com>
2018-02-24 14:08:30 +0400

    Axios postrequest

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,15 +78,38 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updateIngredients);
     };
 
-    purchaseHandler =() => {
+    purchaseHandler = () => {
         this.setState({purchasing: true});
     };
-    purchaseCancelHandler =() => {
+    purchaseCancelHandler = () => {
         this.setState({purchasing: false});
     };
-    purchaseContinueHandler =() => {
-        alert('You continue');
+    purchaseContinueHandler = () => {
+        // alert('You continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Hakob Sukiasyan',
+                address: {
+                    street: 'Svachyan',
+                    zipCode: '0034',
+                    country: 'Armenia'
+                },
+                email: 'test@tast.com'
+            }
+
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
     };
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
